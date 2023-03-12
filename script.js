b3d7c802c2e5534e337c8985b9115beab39fb7d6
adailtondev <adailtonh.dev@gmail.com>
2023-03-12 13:47:00 -0300

    change in button 'ver mais'

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var textoSobreNos = document.querySelector('.texto-sobre')
 botaoVerMais.addEventListener('click', () => {
     if (textoSobreNos.style.height === 'auto') {
         textoSobreNos.style.height = '350px'
+        botaoVerMais.innerHTML = 'Ver mais'
     }else {
         textoSobreNos.style.height = 'auto'
         botaoVerMais.innerHTML = 'Ver menos'
